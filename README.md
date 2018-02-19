ce329c186058f1b5df5f685764b3ccbab1fde63a
AmbulanceMan <duanganucha@hotmail.com>
2018-02-19 21:43:36 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# simple-iot-protocol
+
+![alt text](http://code.isaranu.com/src/img/nodejs-iot-simple-protocol-dashboard/simple-protocol-1.png)
+
+Make simple IoT protocol with Node.js (TCP connection)<br> 
+and chart data plotting with Chart.js. <br>
+Bulid on Google Cloud platform service.<br>
+<br>
+read more details in post at : http://code.isaranu.com/node/2017/nodejs-iot-simple-protocol-dashboard.php
+<br>
+<br>
+Contact<br>
+Facebook : Isaranu Janthong<br>
+Line ID : @codeisaranu<br>
+Website : http://code.isaranu.com<br>
+Youtube channel : Isaranu Janthong<br>
---
+++ arduino libraries/sendtomyprotocol/sendtomyprotocol.ino
@@ -0,0 +1,45 @@
+/*
+Code for Internet of things device
+connect to server.
+Coder : Isaranu Janthong
+Created : 2017.Nov.11
+*/
+
+#include <myprotocol.h>
+
+const char *ssid = "your-ssid name";
+const char *passw = "your-password";
+
+String response; 
+float value;
+
+myprotocol myiot;
+
+void setup() {
+
+  Serial.begin(115200);
+  bool conn = myiot.begin(ssid, passw);
+
+  if(conn){
+      Serial.println("myprotocol connected.");
+    }else{
+      Serial.println("re-connect again.");
+     }
+
+  response = myiot.sayhi();
+  Serial.print("Are you ready ? :" + String(response)); 
+  
+  response = myiot.getVersion();
+  Serial.println("myprotocol library version is " + String(response));  
+}
+
+void loop() {
+  
+  response = "";
+  
+  value = random(40,50);
+  response = myiot.WriteDashboard(value);
+  Serial.println(response);
+  
+  delay(5000);
+}
